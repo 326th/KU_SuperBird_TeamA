97f786534bc6182e22e3b5a721dc4fbea60ece53
326th <minecraftskinman@gmail.com>
2021-04-22 06:14:41 +0700

    add comments and auto allign triggers

--- Assets/Scripts/PipeController.cs
+++ Assets/Scripts/PipeController.cs
@@ -7,8 +7,15 @@ public class PipeController : MonoBehaviour
     GameObject pipeBody;
     GameObject pipeHead;
     private float headHeight = 0.2f;
+
+    /// <summary>
+    /// This Function set the Pipe to that height to any given point relative to it's parent.
+    /// </summary>
+    /// <param name="height">How tall do you want the pipe to be (in meters).</param>
+    /// <param name="pipeBottom">Pipe's starting location Relative to it's parent.</param>
     public void SetPipeHeight(float height,Vector3 pipeBottom)
     {
+        
         BoxCollider[] childs = gameObject.GetComponentsInChildren<BoxCollider>();
         pipeBody = childs[0].gameObject;
         pipeHead = childs[1].gameObject;
@@ -18,6 +25,5 @@ public class PipeController : MonoBehaviour
         pipeBody.transform.position += pipeBottom;
         pipeHead.transform.localPosition = Vector3.zero;
         pipeHead.transform.position += ((Vector3.up * ((height)- headHeight) ) + new Vector3(0,pipeBody.transform.position.y,0));
-        print(pipeBody.transform.position);
     }
 }
--- Assets/Scripts/PipePairController.cs
+++ Assets/Scripts/PipePairController.cs
@@ -6,13 +6,23 @@ public class PipePairController : MonoBehaviour
 {
     PipeController topPipe;
     PipeController bottomPipe;
-    public void SetPipeHeight(float maxRange, float pipeGap, float offset)
+    GameObject trigger;
+    /// <summary>
+    /// Set the pipe pair to face each other vertically with gap in the middle.
+    /// </summary>
+    /// <param name="maxRange">Total length of both pipes including the gap (in meters).</param>
+    /// <param name="pipeGap">The length of the gap between both pipes(in meters).</param>
+    /// <param name="gapOffset">how far the gap wull go up or down (in meters).</param>
+    public void SetPipeHeight(float maxRange, float pipeGap, float gapOffset)
     {
-        PipeController[] pipes = gameObject.GetComponentsInChildren<PipeController>();
-        bottomPipe = pipes[0];
-        topPipe = pipes[1];
-        bottomPipe.SetPipeHeight(((maxRange - pipeGap) + (offset * 2)) / 4, Vector3.down * maxRange / 2);
-        topPipe.SetPipeHeight(((maxRange - pipeGap) - (offset * 2)) / 4, Vector3.down * maxRange / 2);
+        Transform[] transforms = gameObject.GetComponentsInChildren<Transform>();
+        bottomPipe = transforms[1].gameObject.GetComponent<PipeController>();
+        topPipe = transforms[5].gameObject.GetComponent<PipeController>();
+        trigger = transforms[9].gameObject;
+        bottomPipe.SetPipeHeight(((maxRange - pipeGap) + (gapOffset * 2)) / 4, Vector3.down * maxRange / 2);
+        topPipe.SetPipeHeight(((maxRange - pipeGap) - (gapOffset * 2)) / 4, Vector3.down * maxRange / 2);
         topPipe.gameObject.transform.Rotate(180, 0, 0);
+        trigger.transform.localScale = new Vector3(1, pipeGap, 1);
+        trigger.transform.position += Vector3.up * gapOffset;
     }
 }
